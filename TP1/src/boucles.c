3b406473c26b3f771e1fae278bde5fb44ada6676
tailan bebin <bebintailan88@gmail.com>
2025-10-02 12:04:28 +0200

    TP_ProgC: première sauvegarde

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -7,5 +7,42 @@ int main(void){
         scanf("%d", &compteur);
     } while (compteur >= 10 || compteur <= 0);
 
+    for (int i = 0; i <= compteur; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i || i==compteur)
+            {
+                printf("*");
+            }
+            else{
+                printf("#");
+            };
+            
+        }
+        printf("\n");
+        
+    }
+    int i=0;
+    while (i<=compteur)
+    {
+        int j=0;
+        while (j<=i)
+        {
+            if (j == 0 || j == i || i==compteur)
+            {
+                printf("*");
+            }
+            else{
+                printf("#");
+            };
+            j++;
+        }
+        printf("\n");
+        i++;
+        
+    }
+    
+    
 
 }
