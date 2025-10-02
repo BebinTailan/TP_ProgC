c8823cbb765197fbc4d2ef496d1f69a917ce2262
tailan bebin <bebintailan88@gmail.com>
2025-10-02 14:58:43 +0200

    TP_ProgC: première sauvegarde

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+    printf("Addition : %d\n", a + b);
+    printf("Soustraction : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Modulo (reste division de a par b) : %d\n", a % b);
+    printf("a est-il egal a b : %d\n", a == b);       
+    printf("a est-il superieur a b : %d\n", a > b);  
+
+    return 0;
+}
 
